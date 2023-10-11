cc0f6c291b45ba014ed4ce1d7b5c99853d22ceb5
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-10-11 12:33:45 -0600

    Feat(inputs): Set inputs.

--- main.rb
+++ main.rb
@@ -8,3 +8,9 @@ puts "- Obese Class I 30-35"
 puts "- Obese Class II 35-40"
 puts "- Obese Class III > 40"
 puts "Body Mass Index (BMI) Calculator"
+print "Height(cm): "
+height = gets.chomp.to_f
+print "Weight(kg): "
+weight = gets.chomp.to_f
+bmi = weight / (height/100)**2
+puts "BMI = #{bmi}"
